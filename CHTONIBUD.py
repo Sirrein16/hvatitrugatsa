e42c58c69d2d483366157f37a69a93790fb07690
platonl0x <166791360+platonl0x@users.noreply.github.com>
2024-04-12 11:49:51 +0500

    Update CHTONIBUD.py

--- CHTONIBUD.py
+++ CHTONIBUD.py
@@ -1,3 +1,11 @@
 from pygame import*
-#susus
+
 okno = display.set_mode((600,600))
+game = True
+
+while game:
+  for e.type == event.get():
+    if e.type == QUIT:
+      game = False
+  fps.tick(60)
+  display.update()
